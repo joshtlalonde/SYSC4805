9be069fc18e9d79dcfddb631e88750d51ced0ea5
Josh <joshtlalonde@cmail.carleton.ca>
2023-11-14 16:02:50 -0500

    Update Motors setspeed

--- Motors/Motors.cpp
+++ Motors/Motors.cpp
@@ -1,15 +1,15 @@
 #include "Motors.h"
 
-Motors *Motors::motorsInstance = nullptr;
+// Motors *Motors::motorsInstance = nullptr;
 
 Motors::Motors(int rightSpeedPin, int rightDirPin, int leftSpeedPin, int leftDirPin) : motorRight(PWM_DIR, rightSpeedPin, rightDirPin), motorLeft(PWM_DIR, leftSpeedPin, leftDirPin)
 {
-    motorsInstance = this;
+    // motorsInstance = this;
 }
 
-int Motors::motorsOn()
+void Motors::motorsOn()
 {
-    motorRight.setSpeed(255);
+    motorRight.setSpeed(-255);
     motorLeft.setSpeed(255);
 }
 
@@ -24,11 +24,11 @@ void Motors::turn(int side)
     if (side == 0)
     { // 0 for right turn, 1 for left turn
         motorRight.setSpeed(255);
-        motorLeft.setSpeed(0);
+        motorLeft.setSpeed(255);
     }
     else
     {
-        motorRight.setSpeed(0);
-        motorLeft.setSpeed(255);
+        motorRight.setSpeed(-255);
+        motorLeft.setSpeed(-255);
     }
 }
